7682d1d619fddedfccf864573db2839531c7e0b8
Xi Chen <511272827@qq.com>
2017-04-04 23:03:22 +0800

    luogu exercise
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ _luogu/p1421.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h> 
+
+int main()
+{
+	int a, b;
+	int c;
+
+	scanf("%d%d", &a, &b);
+	c = (10 * a + b) / 19;
+	
+	printf("%d\n", c);
+	return 0;
+}
