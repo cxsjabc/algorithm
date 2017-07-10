cbd19da57f2d113cbea523d8b71c31e11663f569
Xi Chen <511272827@qq.com>
2017-07-10 21:58:36 +0800

    binary search
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ _algo_jingjie/test_hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <math.h> 
+#include <assert.h> 
+
+#include "common_local.h"
+
+#ifndef LOG_HERE
+#define LOG_HERE	printf("[%s@%d]\n", __func__, __LINE__);
+#endif
+
+int main()
+{
+
+	return 0;
+}
