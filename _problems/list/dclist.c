d056fbf22e193e2ed07257eca43aad96162cd2a5
Xi Chen <511272827@qq.com>
2018-06-19 22:52:36 +0800

    update list
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ _problems/list/dclist.c
@@ -0,0 +1,16 @@
+#include "common_local.h"
+
+typedef struct _dclistnode
+{
+	int		v;
+	struct _dclistnode	*next;
+	struct _dclistnode	*prev;
+}dclistnode;
+
+void	dclist_insert(dclistnode *node)
+
+int main()
+{
+    printf("%d\n", 0);
+    return 0;
+}
---
+++ _problems/src1/eight_queen.c
@@ -0,0 +1,20 @@
+#include "common_local.h"
+// todo
+
+// the queen on specified line
+int vis[8];
+
+void eight_queen(int n)
+{
+	int i;
+
+	for(i = 0; i < 8; ++i) {
+		
+	}	
+}
+
+int main()
+{
+	eight_queen(0);
+    return 0;
+}
