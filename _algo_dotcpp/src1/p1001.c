91640dec978548d10ec3c966682f5f7de1742d0d
Xi Chen <511272827@qq.com>
2018-07-15 20:48:02 +0800

    dotcpp
    Signed-off-by: Xi Chen <511272827@qq.com>

---
+++ _algo_dotcpp/src1/p1001.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h> 
+
+char *s = "**************************\nHello World!\n**************************";
+
+int main()
+{
+	printf("%s\n", s);
+	return 0;
+}
---
+++ _algo_dotcpp/src1/p1001_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h> 
+
+char *s = "**************************\n\
+Hello World!\n\
+**************************";
+
+int main()
+{
+	printf("%s\n", s);
+	return 0;
+}
