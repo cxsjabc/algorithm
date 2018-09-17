77ab0acdcaf02f3522a6353f74c3ac742a66e717
Xi Chen <511272827@qq.com>
2018-09-17 22:43:55 +0800

    bin sort tree insert
    Signed-off-by: Xi Chen <511272827@qq.com>

--- _algo_yibuyibu_write_algo/src1/bin_sort_tree.c
+++ _algo_yibuyibu_write_algo/src1/bin_sort_tree.c
@@ -55,6 +55,28 @@ pbin_sort_node bin_sort_tree_find(pbin_sort_tree tree, int v)
 		return bin_sort_tree_find(tree->right, v);
 }
 
+int	bin_sort_tree_insert(pbin_sort_tree *pt, int v)
+{
+	pbin_sort_node node;
+
+	if(*pt == NULL) {
+		node = (pbin_sort_node)malloc(sizeof(bin_sort_node));
+		if(!node)
+			return 0;
+		node->v = v;
+		node->left = node->right = NULL;
+		*pt = node; 
+		return 1;
+	} else {
+		if(v == (*pt)->v)
+			return 0;
+		else if(v < (*pt)->v)
+			return bin_sort_tree_insert(&((*pt)->left), v);
+		else
+			return bin_sort_tree_insert(&((*pt)->right), v);
+	}
+}
+
 int main()
 {
 	pbin_sort_tree tree;
@@ -66,5 +88,9 @@ int main()
 	PP(bin_sort_tree_find(tree, 9));
 	PP(bin_sort_tree_find(tree, 1));
 
+	PD(bin_sort_tree_insert(&tree, 3));
+	PD(bin_sort_tree_insert(&tree, 10));
+	PD(bin_sort_tree_insert(&tree, 7));
+	PD(bin_sort_tree_insert(&tree, 5));
     return 0;
 }
